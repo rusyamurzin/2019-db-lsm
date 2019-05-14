5297956d4aec3017ab731c358b1081111a5974bc
rusyamurzin <scretch235@mail.ru>
2019-05-14 11:27:56 +0300

    fix name and class exception

---
+++ src/main/java/ru/mail/polis/murzin/CellReadRuntimeException.java
@@ -0,0 +1,7 @@
+package ru.mail.polis.murzin;
+
+public class CellReadRuntimeException extends RuntimeException {
+    CellReadRuntimeException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/ru/mail/polis/murzin/FileTable.java
+++ src/main/java/ru/mail/polis/murzin/FileTable.java
@@ -183,7 +183,7 @@ public class FileTable implements Table, Closeable {
                 try {
                     return cellAt(next++);
                 } catch (IOException e) {
-                    throw new MyRuntimeException("Unable to read cell at position " + next, e);
+                    throw new CellReadRuntimeException("Unable to read cell at position " + next, e);
                 }
             }
         };
@@ -193,12 +193,6 @@ public class FileTable implements Table, Closeable {
     public void close() throws IOException {
         if (fileChannel != null) {
             fileChannel.close();
-        }
-    }
-
-    private class MyRuntimeException extends RuntimeException {
-        MyRuntimeException(final String message, final Throwable cause) {
-            super(message, cause);
         }
     }
 }
